f8a498565023cc1350adece651f969c903628735
kazz900 <kazz9000@gmail.com>
2023-09-03 22:27:46 +0900

    김화범 게시판 작업물
    김화범 게시판 작업물

--- src/main/java/post/controller/PostNewPostServlet.java
+++ src/main/java/post/controller/PostNewPostServlet.java
@@ -2,14 +2,23 @@ package post.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import standardpost.model.service.StandardPostService;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import common.FileNameChange;
+import post.model.service.PostSerivce;
+import post.model.vo.Post;
 import standardpost.model.vo.StandardPost;
+import tradepost.model.vo.TradePost;
 
 /**
  * Servlet implementation class StandardPostNewPostViewServlet
@@ -26,6 +35,66 @@ public class PostNewPostServlet extends HttpServlet {
 		// TODO Auto-generated constructor stub
 	}
 
+//	// 게시글 원글 등록 처리용 컨트롤러 : 파일업로드가 있는 전송인 경우임
+//	
+//	//1. multipart 방식으로 인코딩되어서 전송왔는지 확인
+//	//아니면 에러 페이지를 내보냄
+//	RequestDispatcher view = null;
+//	if(!ServletFileUpload.isMultipartContent(request)) {
+//		view = request.getRequestDispatcher("views/common/error.jsp");
+//		request.setAttribute("message", "form의 enctype='multipart/form-data' 속성 누락됨.");
+//		view.forward(request, response);
+//	}
+//	
+//	//2. 업로드할 파일의 용량 제한 설정 : 10메가바이트로 제한한다면
+//	int maxSize = 1024 * 1024 * 10;
+//	
+//	//3. 업로드되는 파일의 저장 폴더 지정
+//	String savePath = request.getSession().getServletContext().getRealPath("/resources/board_upfiles");
+//	//request.getSession().getServletContext()  => "/first"  + 뒤에 하위 폴더 경로 추가함
+//	
+//	//4. request 를 MultipartRequest 로 변환해야 함
+//	//MultipartRequest 클래스는 외부 라이브러리를 사용해야 함 : cos.jar 사용한 경우
+//	//MultipartRequest 객체가 생성되면, 자동으로 지정 폴더에 업로드된 파일이 저장됨
+//	MultipartRequest mrequest = new MultipartRequest(request, savePath, maxSize, 
+//			"UTF-8", new DefaultFileRenamePolicy());
+//	
+//	//5. 데이터베이스 board 테이블에 기록할 값 추출
+//	//mrequest 사용해야 함 (request 사용 못 함)
+//	Board board = new Board();
+//	
+//	board.setBoardTitle(mrequest.getParameter("title"));
+//	board.setBoardWriter(mrequest.getParameter("writer"));
+//	board.setBoardContent(mrequest.getParameter("content"));
+//	
+//	//6. 업로드된 원본 파일이름 추출
+//	String originalFileName = mrequest.getFilesystemName("upfile");
+//	board.setBoardOriginalFileName(originalFileName);
+//	
+//	//7. 폴더에 저장된 파일의 이름바꾸기 처리
+//	// 저장 폴더에 같은 이름의 파일이 있을 경우를 대비하기 위함
+//	// "년월일시분초.확장자" 형식으로 변경할 것임
+//	if(originalFileName != null) {
+//		//업로드된 파일이 있을 때만 파일명 바꾸기 실행함
+//		
+//		String renameFileName = FileNameChange.change(
+//				originalFileName, savePath, "yyyyMMddHHmmss");
+//		
+//		board.setBoardRenameFileName(renameFileName);			
+//	}  //업로드된 파일이 있다면...
+//	
+//	//모델 서비스 메소드로 전달하고 결과받기
+//	int result = new BoardService().insertOriginBoard(board);
+//	
+//	//받은 결과로 성공/실패 페이지 내보내기
+//	if(result > 0) {
+//		//서블릿에서 서블릿을 실행함
+//		response.sendRedirect("/first/blist?page=1");
+//	}else {
+//		view = request.getRequestDispatcher("views/common/error.jsp");
+//		request.setAttribute("message", "새 게시 원글 등록 실패.");
+//		view.forward(request, response);
+//	}
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
@@ -33,26 +102,76 @@ public class PostNewPostServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		int result = 0;
-		StandardPostService spService = new StandardPostService();
-
-		request.setCharacterEncoding("utf-8");
-
-		String postContent = request.getParameter("post-content");
-		// TODO : MEMBER VALIDITY CHECK
-		int memberSeq = Integer.parseInt(request.getParameter("memberseq"));
+		RequestDispatcher view = null;
+		Post p = null;
+		PostSerivce pService = new PostSerivce();
 		
-		StandardPost sp = new StandardPost();
-		sp.setMemberSeq(memberSeq);
-		sp.setPostContent(postContent);
+		//1. multipart 방식으로 인코딩되어서 전송왔는지 확인
+		if (!ServletFileUpload.isMultipartContent(request)) {
+			view = request.getRequestDispatcher("views/common/error.jsp");
+			request.setAttribute("message", "form의 enctype='multipart/form-data' 속성 누락됨.");
+			view.forward(request, response);
+		}
 		
+		//2. 업로드할 파일의 용량 제한 설정 : 10메가바이트로 제한한다면
+		int maxSize = 1024 * 1024 * 10;
+		
+		//3. 업로드되는 파일의 저장 폴더 지정
+		String savePath = request.getSession().getServletContext().getRealPath("/resources/postupfiles");
+		//request.getSession().getServletContext()  => "/first"  + 뒤에 하위 폴더 경로 추가함
+		
+		
+		//4. request 를 MultipartRequest 로 변환해야 함
+		//MultipartRequest 클래스는 외부 라이브러리를 사용해야 함 : cos.jar 사용한 경우
+		//MultipartRequest 객체가 생성되면, 자동으로 지정 폴더에 업로드된 파일이 저장됨
+		MultipartRequest mrequest = new MultipartRequest(request, savePath, maxSize, "UTF-8", new DefaultFileRenamePolicy());
+		
+		//6. 업로드된 원본 파일이름 추출
+		String originalFileName = mrequest.getFilesystemName("upfile");
+		
+		//7. 폴더에 저장된 파일의 이름바꾸기 처리
+		// 저장 폴더에 같은 이름의 파일이 있을 경우를 대비하기 위함
+		// "년월일시분초.확장자" 형식으로 변경할 것임
+		
+		// GET POST INFORMATIONS
+		int memberSeq = Integer.parseInt(mrequest.getParameter("memberseq"));
+		System.out.println(memberSeq);
+		String postType = mrequest.getParameter("post-type");
+		String postContent = mrequest.getParameter("post-content");
+		
+		if(postType == null) {
+			System.out.println("");
+		}
+		
+		// 일반게시글일떄
+		if (postType.equals("standardpost")) {
+			p = new StandardPost();
+			p.setMemberSeq(memberSeq);
+			p.setPostContent(postContent);
+			p.setOriginalFileName(originalFileName);
+			if(originalFileName != null) {
+				String renameFileName = FileNameChange.change(originalFileName, savePath, "yyyyMMddHHmmss");
+				p.setChangedFileName(renameFileName);
+			}
+		} else { // 중고거래게시글일떄
+			p = new TradePost();
+			p.setMemberSeq(memberSeq);
+			p.setPostContent(postContent);
+			p.setOriginalFileName(originalFileName);
+			if(originalFileName != null) {
+				String renameFileName = FileNameChange.change(originalFileName, savePath, "yyyyMMddHHmmss");
+				p.setChangedFileName(renameFileName);
+			}
+		}
 
-		result = spService.insertStandardPost(sp.getMemberSeq(), sp);
+		result = pService.insertPost(p.getMemberSeq(), p);
 
 		if (result > 0) {
-			//insert successful
-			response.sendRedirect("/petmily/splist");
+			// insert successful
+			String path = "/petmily/plist?memberseq=" + String.valueOf(memberSeq);
+			response.sendRedirect(path);
 		} else {
-			//insert fail
+			// insert fail
 			// TODO : MAKE INSERT FAIL HTML
 		}
 	}
---
+++ src/main/java/reply/exception/ReplyException.java
@@ -0,0 +1,7 @@
+package reply.exception;
+
+public class ReplyException extends Exception{
+	public ReplyException(String message) {
+		super(message);
+	}
+}
