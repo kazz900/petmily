3541e2a754b6e9b62c15bc2d9f3aaa4a69cfce94
sconfire21 <gonzo19924@gmail.com>
2023-09-07 18:42:05 +0900

    버그 수정
    버그 수정

--- src/main/java/admin/controller/AdminTPostSearch.java
+++ src/main/java/admin/controller/AdminTPostSearch.java
@@ -56,6 +56,7 @@ request.setCharacterEncoding("utf-8");
 				
 		RequestDispatcher view = null;
 		
+
 		view = request.getRequestDispatcher("views/admin/AdminTradePostDelete.jsp");
 		request.setAttribute("list", list);
 		request.setAttribute("keyword", keyword);
